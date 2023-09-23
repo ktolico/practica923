2103ccffddc7492755cf9247bd6bc22ccd8460a6
ktolico <130526956+ktolico@users.noreply.github.com>
2023-09-23 12:37:28 -0500

    practice2

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,6 +2,24 @@ console.log("Entro index.js");
 
 const btnBlue = document.getElementById("btnBlue");
 
-btnBlue.addEventListener("click", function () {
-    console.log("click en el boton azul!!!");
+// btnBlue.addEventListener("click", function () {
+//     console.log("click en el boton azul!!!");
+// });
+
+function clickBotonAzul() {
+    console.log("click en boton");
+}
+
+function OverButtonAzul(){
+    console.log("Entro evento over");
+}
+
+function changeBackgroundColorPage(color) {
+    document.body.style.backgroundColor = color;
+}
+
+btnBlue.addEventListener("click", ()=> {
+    changeBackgroundColorPage("Blue")
 });
+
+btnBlue.addEventListener("mouseover", OverButtonAzul);
