48c5495213491040d6a3924faa9519b9a8aff9bd
ktolico <130526956+ktolico@users.noreply.github.com>
2023-09-23 11:33:23 -0500

    hour30minutoslater

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,7 @@
 console.log("Entro index.js");
+
+const btnBlue = document.getElementById("btnBlue");
+
+btnBlue.addEventListener("click", function () {
+    console.log("click en el boton azul!!!");
+});
